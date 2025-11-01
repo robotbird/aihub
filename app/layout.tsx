40f9920888318a1f11035d2226a9698b3b9f0d61
robotbird <robotbird@qq.com>
2025-11-01 22:25:09 +0800

    fit:增加谷歌统计代码

--- app/layout.tsx
+++ app/layout.tsx
@@ -84,6 +84,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HK024B0CJD"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-HK024B0CJD');
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
